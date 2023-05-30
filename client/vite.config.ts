3b53232e74666ddfd345384697b0b10d82504d05
Sergio Marin Sanchez <serms1999@gmail.com>
2023-05-30 19:30:28 +0200

    Use .env for client port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv';
+import path from 'path';
+
+// @ts-ignore
+dotenv.config({path: path.resolve(__dirname, 'src/config/.env')});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // @ts-ignore
+    port: process.env.CLIENT_PORT
+  }
 })
